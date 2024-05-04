0bbb56481e041bad575d0229b7f3b3644b3d5647
Shreyas Deshmukh <shreyasdeshmukh1982@gmail.com>
2024-05-04 19:25:43 +0000

    Sync LeetCode submission - Boats to Save People - Runtime - 55 ms (5.07%), Memory - 45.5 MB (99.98%)

---
+++ 0917-boats-to-save-people/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int count = 0;
+        int left = 0, right = people.size() - 1;
+
+        // while (left <= right) {
+        //     if (people[left] + people[right] <= limit) {
+        //         left++;
+        //     }
+        //     right--;
+        //     count++;
+        // }
+        while(left<=right)
+        {
+            if(people[left]+people[right]<=limit)
+            {
+                count++;
+                left++;
+                right--;
+            }
+            else
+            {
+                count++;
+                right--;
+            }
+        }
+
+        return count;
+    }
+
+};
+
