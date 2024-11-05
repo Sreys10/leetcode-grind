ec20a5deb66b05d7f9422a693d0139cf6f3ad017
Shreyas Deshmukh <shreyasdeshmukh1982@gmail.com>
2024-11-05 08:32:53 +0000

    Sync LeetCode submission - Reverse String - Runtime - 0 ms (100.00%), Memory - 28 MB (15.02%)

--- 0344-reverse-string/solution.cpp
+++ 0344-reverse-string/solution.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-    int n = s.size();
-        for (int i = 0; i < n / 2; ++i) {
-            char temp = s[i];
-            s[i] = s[n - i - 1];
-            s[n - i - 1] = temp;
+    stack<char>st;
+
+    for(char c:s){
+        st.push(c);
     }
+    s.clear();
+
+    while(!st.empty()){
+        s.push_back(st.top());
+        st.pop();
+    }
+
+    
     
     }
 };
