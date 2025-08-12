0d03e120adec5cec26f0ed8bfab3197bffc10bd6
Shreyas Deshmukh <shreyasdeshmukh1982@gmail.com>
2025-08-12 02:56:25 +0000

    Sync LeetCode submission - Two Sum - Runtime - 46 ms (14.30%), Memory - 14.1 MB (68.46%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,26 +1,13 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> numIndexMap;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            
-            // Check if the complement exists in the map
-            if (numIndexMap.find(complement) != numIndexMap.end()) {
-                // Found the pair
-                return { numIndexMap[complement], i };
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j}; 
+                }
             }
-            
-            // Store the current number and its index in the map
-            numIndexMap[nums[i]] = i;
         }
-        
-        // Return an empty vector if no such pair exists (though problem statement guarantees a solution)
-        return {};
+        return {}; 
     }
 };
-
