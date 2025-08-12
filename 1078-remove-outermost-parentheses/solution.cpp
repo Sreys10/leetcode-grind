eced373848bee598b3941db5c68ff9004c7c49d2
Shreyas Deshmukh <shreyasdeshmukh1982@gmail.com>
2025-08-12 16:53:04 +0000

    Sync LeetCode submission - Remove Outermost Parentheses - Runtime - 0 ms (100.00%), Memory - 9 MB (23.84%)

---
+++ 1078-remove-outermost-parentheses/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count=0;
+        string ans="";
+        for(int i=0; i<=s.size(); i++){
+            if(s[i]=='('){
+                if(count>0) ans+=s[i];
+                count++;
+            }
+            
+            else if (s[i]==')'){
+                count--;
+                if(count>0) ans+=s[i];
+            }
+        }
+        return ans;
+    }    
+};
+
+//basically here we made the summation of ( and ) == 0
